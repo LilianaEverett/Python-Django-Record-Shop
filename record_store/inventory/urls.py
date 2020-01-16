bbee995cffe725116c24552db85e033146137455
Liliana Everett <everettliliana@gmail.com>
2020-01-16 15:28:05 +0000

    app created with auth

---
+++ record_store/inventory/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('album/new/', views.album_new, name='album_new'),
+    path('artist/new/', views.artist_new, name='artist_new'),
+    #views is the file
+
+]
